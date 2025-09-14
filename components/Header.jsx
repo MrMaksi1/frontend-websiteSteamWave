4e71f05fb9712a4d4e636eebf5801fd829a1ac22
kotean <partemach@gmail.com>
2025-09-14 21:01:10 +0300

    Header and footer are nearly ready

--- components/Header.jsx
+++ components/Header.jsx
@@ -1,10 +1,9 @@
-﻿// src/components/Header.jsx
-'use client'
+﻿'use client'
 
 import Link from 'next/link'
-import Image from 'next/image'
 import { usePathname } from 'next/navigation'
 import './components.css'
+import HamburgerMenu from "@/components/HamburgerMenu";
 
 export default function Header() {
     const pathname = usePathname()
@@ -14,32 +13,33 @@ export default function Header() {
     }
 
     return (
-        <header className="header-main">
+        <header className="header">
             <div className="header-container">
                 <nav className="nav">
 
-                    <Link href="/" className="nav-logo">
-                        <Image className="nav-logo image" src="/black_256.svg" alt="SteamWave logo" width={320} height={38} priority/>
-                        STEAMWAVE
+                    <Link href="/" className="header-logo">
+                        <span className="header-logo-icon"></span>
+                        {/*STEAMWAVE*/}
                     </Link>
 
-                    <span className="nav-divider" />
+                    {/*<HamburgerMenu />*/}
 
-                    <div className="nav-center">
-                        <ul className="nav-links-list">
-                            <li><Link href="/" className={`${isActive('/') ? 'nav-link active' : 'nav-link'}`}>Главная</Link></li>
-                            <li><Link href="/about" className={`${isActive('/about') ? 'nav-link active' : 'nav-link'}`}>О нас</Link></li>
-                            <li><Link href="/news" className={`${isActive('/news') ? 'nav-link active' : 'nav-link'}`}>Новости</Link></li>
-                            <li><Link href="/rules" className={`${isActive('/rules') ? 'nav-link active' : 'nav-link'}`}>Правила</Link></li>
+                    <span className="header-divider"/>
+
+                    <div className="header-center">
+                        <ul className="header-links-list">
+                            <li><Link href="/" className={`header-link ${isActive('/') ? 'active' : ''}`}>Главная</Link></li>
+                            <li><Link href="/about" className={`header-link ${isActive('/about') ? 'active' : ''}`}>О нас</Link></li>
+                            <li><Link href="/news" className={`header-link ${isActive('/news') ? 'active' : ''}`}>Новости</Link></li>
+                            <li><Link href="/rules" className={`header-link ${isActive('/rules') ? 'active' : ''}`}>Правила</Link></li>
                         </ul>
                     </div>
 
-                    <span className="nav-divider" />
+                    <span className="header-divider"/>
 
-                    <Link href="/profile" className={`${isActive('/profile') ? 'nav-profile active' : 'nav-profile'}`}>
-                        {/*<Image className="dark:invert" src="/next.svg" alt="Next.js logo" width={320} height={38} priority/>*/}
-                        {/*Профиль*/}
-                        <img className="nav-profile-image" src="https://minotar.net/helm/kotean_st/32.png" alt="Profile picture" />
+                    <Link href="/profile" className={`header-profile ${isActive('/profile') ? 'active' : ''}`}>
+                        <img className="header-profile-image" src="https://minotar.net/helm/kotean_st/32.png"
+                             alt="Profile picture"/>
                     </Link>
                 </nav>
             </div>
