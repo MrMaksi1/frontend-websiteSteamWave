a7ec51a628f8145af70808b607eeb191aa2a26e1
kotean <partemach@gmail.com>
2025-11-04 20:00:07 +0300

    Updated News.jsx and Header.jsx. Added BACKEND_URL in config.jsx

--- components/News/News.jsx
+++ components/News/News.jsx
@@ -3,7 +3,7 @@
 import { useEffect, useState } from 'react'
 import styles from './News.module.css'
 import {useRouter} from "next/navigation";
-import {API_URL} from "@/config/config.jsx";
+import {API_URL, BACKEND_URL} from "@/config/config.jsx";
 
 export default function NewsPage() {
     const [news, setNews] = useState([])
@@ -32,7 +32,7 @@ export default function NewsPage() {
 
                 const newsWithFullUrl = data.map(post => ({
                     ...post,
-                    mediaUrl: post.mediaUrl ? `${BACKEND_URL}${post.mediaUrl}` : null,
+                    mediaUrl: post.mediaUrl ? `${BACKEND_URL}/${post.mediaUrl}` : null,
                     fullContentUrl: `/news/${post.id}`
                 }))
 
