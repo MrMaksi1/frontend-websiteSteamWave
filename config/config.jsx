b134a65b8c65e3a7dc3fc5da2cf658b5e0164758
kotean <partemach@gmail.com>
2025-11-07 21:25:40 +0300

    Updated config.jsx

--- config/config.jsx
+++ config/config.jsx
@@ -1,2 +1,2 @@
-﻿export const API_URL = process.env.SMW_PUBLIC_API_URL || 'http://localhost:8080/api'
-export const BACKEND_URL = process.env.SMW_PUBLIC_BACKEND_URL || 'http://localhost:8080'
+﻿export const API_URL = process.env.NEXT_PUBLIC_API_URL || 'http://localhost:8080/api'
+export const BACKEND_URL = process.env.NEXT_PUBLIC_BACKEND_URL || 'http://localhost:8080'
