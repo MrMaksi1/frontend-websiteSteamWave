83d9f1b4b04c4d4345624c2fba3a6baabdb8c844
kotean <partemach@gmail.com>
2025-11-07 21:14:14 +0300

    Updated config.jsx to use URL's from .env

--- config/config.jsx
+++ config/config.jsx
@@ -1,2 +1,2 @@
-﻿export const API_URL = 'https://steamwave.ru/api' || 'http://localhost:8080/api'
-export const BACKEND_URL = 'https://steamwave.ru' || 'http://localhost:8080'
+﻿export const API_URL = process.env.SMW_PUBLIC_API_URL || 'http://localhost:8080/api'
+export const BACKEND_URL = process.env.SMW_PUBLIC_BACKEND_URL || 'http://localhost:8080'
